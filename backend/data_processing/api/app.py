5ed719b9ff9b7641eefe8c3fc69927764ff030e4
GirumZ <girum.zebe@gmail.com>
2024-03-01 11:26:17 +0300

    [Updata]: Added more endpoints to the api

--- backend/data_processing/api/app.py
+++ backend/data_processing/api/app.py
@@ -30,44 +30,75 @@ def execute_query(query, params=None):
         cursor.close()
         connection.close()
 
+# to list all the symptoms
 @app.route('/api/symptoms', methods=['GET'])
 def get_symptoms():
     query = 'SELECT * FROM symptoms'
     symptoms = execute_query(query)
-    return jsonify({'symptoms': symptoms})
+    return jsonify(symptoms)
 
+# to list all questions
 @app.route('/api/questions', methods=['GET'])
 def get_questions():
     query = 'SELECT * FROM questions'
     questions = execute_query(query)
-    return jsonify({'questions': questions})
+    return jsonify(questions)
 
+# to list all the problems
 @app.route('/api/problems', methods=['GET'])
 def get_problems():
     query = 'SELECT * FROM problems'
     problems = execute_query(query)
-    return jsonify({'problems': problems})
+    return jsonify(problems)
 
+# to show a symptom with specific Symptom_id
 @app.route('/api/symptoms/<int:Symptom_id>', methods=['GET'])
 def get_symptoms_id(Symptom_id):
     query = 'SELECT * FROM symptoms WHERE Symptom_id = %s'
     params = (Symptom_id,)
     symptoms = execute_query(query, params)
-    return jsonify({'symptoms': symptoms})
+    return jsonify(symptoms)
 
+# to show a question with specific Symptom_id
 @app.route('/api/questions/<int:Symptom_id>', methods=['GET'])
 def get_questions_id(Symptom_id):
     query = 'SELECT * FROM questions WHERE Symptom_id = %s'
     params = (Symptom_id,)
     questions = execute_query(query, params)
-    return jsonify({'questions': questions})
+    return jsonify(questions)
 
+# to show a problem with specific Question_id
 @app.route('/api/problems/<int:Question_id>', methods=['GET'])
 def get_problems_id(Question_id):
     query = 'SELECT * FROM problems WHERE Question_id = %s'
     params = (Question_id,)
     problems = execute_query(query, params)
-    return jsonify({'problems': problems})
+    return jsonify(problems)
+
+# to show a symptom with specific Sensation type
+@app.route('/api/symptoms/Sensation/<string:Sensation>', methods=['GET'])
+def get_symptoms_sensation(Sensation):
+    query = 'SELECT * FROM symptoms WHERE Sensation = %s'
+    params = (Sensation,)
+    symptoms = execute_query(query, params)
+    return jsonify(symptoms)
+
+# to show a symptom with followup availability
+@app.route('/api/symptoms/Followup_available/<string:Followup_available>', methods=['GET'])
+def get_symptoms_followup(Followup_available):
+    query = 'SELECT * FROM symptoms WHERE Followup_available = %s'
+    params = (Followup_available,)
+    symptoms = execute_query(query, params)
+    return jsonify(symptoms)
+
+# to show a problem with specific Symptom_id
+@app.route('/api/problems/Symptom_id/<int:Symptom_id>', methods=['GET'])
+def get_problems_question_id(Symptom_id):
+    query = 'SELECT * FROM problems WHERE Symptom_id = %s'
+    params = (Symptom_id,)
+    problems = execute_query(query, params)
+    return jsonify(problems)
+
 
 if __name__ == '__main__':
     app.run()
