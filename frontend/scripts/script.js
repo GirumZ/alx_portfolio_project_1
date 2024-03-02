3e9509fbb2912dafd1464a82ecee8c2fdc8c00ce
GirumZ <girum.zebe@gmail.com>
2024-03-02 16:04:40 +0300

    [Update]: Added gerQueryParam and createButtons functions to the js script

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -3,11 +3,18 @@ function loadTrp() {
     window.location.href = "troubleshoot.html";
 }
 
-function getData() {
+// A function to load the followup page
+function loadFlup(sensation) {
+	// Redirects to the folloup page with specific parameter
+	window.location.href = 'followup.html?sensation=' + sensation;
+}
+
+function getData(sensation) {
 
     const flist = []	
     // Replace 'https://api.example.com/data' with the actual API endpoint
-    fetch('http://localhost:5000/api/symptoms/Sensation/hear')
+    apiurl = 'http://localhost:5000/api/symptoms/Sensation/' + sensation
+    return fetch(apiurl)
         .then(response => {
             // Check if the request was successful (status code 2xx)
             if (!response.ok) {
@@ -16,16 +23,28 @@ function getData() {
             // Parse the JSON response
             return response.json();
         })
-        .then(data => {
-            // Handle the data from the API
-            console.log('Data from the API:', data);
-	    for (let i = 0; i < data.length; i++) {
-		    flist.push(data[i].Symptom);
-	    }
-	    console.log(flist);
-        })
         .catch(error => {
             // Handle errors
             console.error('Error fetching data:', error);
         });
+}
+
+// Function to get query parameters from the URL
+function getQueryParam(name) {
+	const urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get(name);
+}
+
+function createButtons(buttonNames) {
+	var container = document.getElementById("buttonsContainer");
+    
+	for (let i = 0; i < buttonNames.length; i++) {
+		var button = document.createElement("button");
+		button.innerHTML = buttonNames[i].Symptom;
+
+		// event listner to be added here
+
+		container.appendChild(button);
+	}
+	
 }
